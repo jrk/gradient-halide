e52f4b55322f343a3e3a74cbbbc10b5dfb786bed
Tzu-Mao Li <tzumao@mit.edu>
2017-08-21 00:12:09 -0700

    basic test case

---
+++ gradient_test/basic.cpp
@@ -0,0 +1,29 @@
+// g++ basic.cpp -g -I ../include -L ../bin -lHalide -o basic -std=c++11
+// DYLD_LIBRARY_PATH=../bin ./basic
+
+// The only Halide header file you need is Halide.h. It includes all of Halide.
+#include "Halide.h"
+
+// We'll also include stdio for printf.
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    Halide::Param<float> factor(1.333);
+    Halide::Func gradient;
+    Halide::Var x, y;
+    Halide::Expr e = Halide::forward_ad(factor * (x + y), factor);
+    gradient(x, y) = e;
+    Halide::Buffer<float> output = gradient.realize(800, 600);
+    for (int j = 0; j < output.height(); j++) {
+        for (int i = 0; i < output.width(); i++) {
+            if (output(i, j) != (i + j)) {
+                printf("Something went wrong!\n"
+                       "Pixel %d, %d was supposed to be %f, but instead it's %f\n",
+                       i, j, float(i + j), output(i, j));
+                return -1;
+            }
+        }
+    }
+    printf("Success!\n");
+    return 0;
+}
