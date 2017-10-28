310809fbd42bb56a68e626371f036bd83c3dcd5c
Tzu-Mao Li <tzumao@mit.edu>
2017-10-28 17:14:12 -0400

    bilateral layer with multiple channels & guide image

--- apps/bilateral_layer/bilateral_layer_generator.cpp
+++ apps/bilateral_layer/bilateral_layer_generator.cpp
@@ -6,39 +6,53 @@ using namespace Halide;
 
 class BilateralLayer : public Halide::Generator<BilateralLayer> {
 public:
-    Input<Buffer<float>>  input{"input", 2};
-    Input<Buffer<float>>  filter{"filter", 3};
-    Input<Buffer<float>>  bias{"bias", 1};
-    Input<Buffer<float>>  compare{"compare", 2};
+    Input<Buffer<float>>  input{"input", 4};       // x, y, channel, batch size
+    Input<Buffer<float>>  guide{"guide", 3};       // x, y, batch size
+    Input<Buffer<float>>  filter{"filter", 5};     // x, y, z offset, input channel, output channel
+    Input<Buffer<float>>  bias{"bias", 2};         // z offset, channel
+    Input<Buffer<float>>  adjoint{"adjoint", 4};   // x, y, channel, batch size
 
-    Output<Buffer<float>> output{"output", 2};
-    Output<Buffer<float>> d_filter{"d_filter", 3};
-    Output<Buffer<float>> d_bias{"d_bias", 1};
+    Output<Buffer<float>> output{"output", 4};     // x, y, channel, batch size
+    Output<Buffer<float>> d_input{"d_input", 4};   // same as input
+    Output<Buffer<float>> d_guide{"d_guide", 3};   // same as guide
+    Output<Buffer<float>> d_filter{"d_filter", 5}; // same as filter
+    Output<Buffer<float>> d_bias{"d_bias", 2};     // same as bias
 
     void generate() {
         /* THE ALGORITHM */
 
-        Var x("x"), y("y"), z("z"), n("n");
+        Var x("x"), y("y"), z("z"), n("n"), ci("ci"), co("co");
 
         // TODO: should we downsample the input here?
 
         // Offsets the input by different biases and applies ReLU
+        // Do this for each channel
+        Func f_input("input");
+        f_input(x, y, ci, n) = input(x, y, ci, n);
+        Func f_guide("guide");
+        f_guide(x, y, n) = guide(x, y, n);
         Func f_bias("bias");
-        f_bias(x) = bias(x);
+        f_bias(z, ci) = bias(z, ci);
         Func f_offset("offset");
-        f_offset(x, y, z) = max(0.f, input(x, y) + f_bias(z));
+        // x, y, z offset, input channel, batch size
+        f_offset(x, y, z, ci, n) = max(0.f, f_input(x, y, ci, n) + f_bias(z, ci));
         // Perform 3D filtering in the offseted space
-        RDom r(filter.dim(0).min(), filter.dim(0).extent(),
-               filter.dim(1).min(), filter.dim(1).extent(),
-               filter.dim(2).min(), filter.dim(2).extent());
+        // Again, do this for each channel
+        // We assume the z offset part is fully-connected 
+        // (i.e. filter.dim(2).extent() == bias.dim(1).extent())
+        RDom r(filter.dim(0).min(), filter.dim(0).extent(),  // x
+               filter.dim(1).min(), filter.dim(1).extent(),  // y
+               filter.dim(2).min(), filter.dim(2).extent(),  // z offset
+               filter.dim(3).min(), filter.dim(3).extent()); // input channel
         Func f_filter("filter");
-        f_filter(x, y, z) = filter(x, y, z);
+        f_filter(x, y, z, ci, co) = filter(x, y, z, ci, co);
         Func f_conv("conv");
-        f_conv(x, y, z)  = 0.f;
-        f_conv(x, y, z) += f_filter(r.x, r.y, r.z) * f_offset(x + r.x, y + r.y, r.z);
+        f_conv(x, y, z, co, n)  = 0.f;
+        f_conv(x, y, z, co, n) += f_filter(r[0], r[1], r[2], r[3], co) *
+                                  f_offset(x + r[0], y + r[1], r[2], r[3], n);
         // Slice the result back to 2D
         // Find the coordinate in z
-        Expr gz = clamp(input(x, y), 0.0f, 1.0f) * (filter.dim(2).extent() - 1);
+        Expr gz = clamp(f_guide(x, y, n), 0.0f, 1.0f) * (filter.dim(2).extent() - 1);
         // Floor voxel
         Expr fz = cast<int>(floor(gz) - 0.5f);
         // Ceil voxel
@@ -47,48 +61,74 @@ public:
         Expr wz = gz - fz;
         // Linear interpolation
         Func f_output("output");
-        f_output(x, y) = f_conv(x, y, fz) * (1.f - wz) + f_conv(x, y, cz) * wz;
+        f_output(x, y, co, n) = f_conv(x, y, fz, co, n) * (1.f - wz) +
+                                f_conv(x, y, cz, co, n) * wz;
 
-        RDom r_output(compare);
-        // RDom r_output(0, 64, 0, 64);
-        Func f_loss("loss");
-        f_loss(x) = 0.f;
-        f_loss(x) += f_output(r_output.x, r_output.y);
-        Derivative d = propagate_adjoints(f_loss);
+        RDom r_output(adjoint);
+        Derivative d = propagate_adjoints(f_output, adjoint,
+                                          {{adjoint.dim(0).min(), adjoint.dim(0).max()},
+                                           {adjoint.dim(1).min(), adjoint.dim(1).max()},
+                                           {adjoint.dim(2).min(), adjoint.dim(2).max()},
+                                           {adjoint.dim(3).min(), adjoint.dim(3).max()}});
         std::map<FuncKey, Func> adjoints = d.adjoints;
+        Func f_d_input = adjoints[FuncKey{f_input.name(), -1}];
+        Func f_d_guide = adjoints[FuncKey{f_guide.name(), -1}];
         Func f_d_filter = adjoints[FuncKey{f_filter.name(), -1}];
         Func f_d_bias = adjoints[FuncKey{f_bias.name(), -1}];
-        print_func(f_d_bias);
 
-        output(x, y) = f_output(x, y);
-        d_filter(x, y, z) = f_d_filter(x, y, z);
-        d_bias(x) = f_d_bias(x);
+        output(x, y, co, n) = f_output(x, y, co, n);
+        d_input(x, y, ci, n) = f_d_input(x, y, ci, n);
+        d_guide(x, y, n) = f_d_guide(x, y, n);
+        d_filter(x, y, z, ci, co) = f_d_filter(x, y, z, ci, co);
+        d_bias(z, ci) = f_d_bias(z, ci);
 
         /* THE SCHEDULE */
         if (auto_schedule) {
             // Provide estimates on the input image
             input.dim(0).set_bounds_estimate(0, 67);
             input.dim(1).set_bounds_estimate(0, 67);
+            input.dim(2).set_bounds_estimate(0, 1);
+            input.dim(3).set_bounds_estimate(0, 1);
 
             filter.dim(0).set_bounds_estimate(0, 3);
             filter.dim(1).set_bounds_estimate(0, 3);
             filter.dim(2).set_bounds_estimate(0, 8);
+            filter.dim(3).set_bounds_estimate(0, 1);
+            filter.dim(4).set_bounds_estimate(0, 1);
 
             bias.dim(0).set_bounds_estimate(0, 8);
+            bias.dim(1).set_bounds_estimate(0, 1);
 
-            compare.dim(0).set_bounds_estimate(0, 64);
-            compare.dim(1).set_bounds_estimate(0, 64);
+            adjoint.dim(0).set_bounds_estimate(0, 64);
+            adjoint.dim(1).set_bounds_estimate(0, 64);
+            adjoint.dim(2).set_bounds_estimate(0, 1);
+            adjoint.dim(3).set_bounds_estimate(0, 1);
 
             // Provide estimates on the pipeline f_ReLU
-            output.estimate(x, 0, 64)
-                  .estimate(y, 0, 64);
+            output.estimate(x , 0, 64)
+                  .estimate(y , 0, 64)
+                  .estimate(co, 0, 1)
+                  .estimate(n , 0, 1);
 
-            d_filter.estimate(x, 0, 3)
-                    .estimate(y, 0, 3)
-                    .estimate(z, 0, 8);
+            d_input.estimate(x , 0, 67)
+                   .estimate(y , 0, 67)
+                   .estimate(ci, 0, 1)
+                   .estimate(n , 0, 1);
 
-            d_bias.estimate(x, 0, 1);
+            d_guide.estimate(x, 0, 67)
+                   .estimate(y, 0, 67)
+                   .estimate(n, 0, 1);
+
+            d_filter.estimate(x , 0, 3)
+                    .estimate(y , 0, 3)
+                    .estimate(z , 0, 8)
+                    .estimate(ci, 0, 1)
+                    .estimate(co, 0, 1);
+
+            d_bias.estimate(z , 0, 1)
+                  .estimate(ci, 0, 1);
         } else {
+            f_offset.compute_root();
             f_output.compute_root();
             f_conv.compute_root();
             adjoints[FuncKey{f_output.name(), -1}].compute_root();
--- apps/bilateral_layer/process.cpp
+++ apps/bilateral_layer/process.cpp
@@ -10,46 +10,71 @@ using namespace Halide::Tools;
 using namespace Halide::Runtime;
 
 int main(int argc, char **argv) {
-    Buffer<float> input(67, 67);
-    Buffer<float> filter(3, 3, 8);
-    Buffer<float> bias(8);
-    Buffer<float> target(64, 64);
+    Buffer<float> input(67, 67, 1, 1);
+    Buffer<float> guide(67, 67, 1);
+    Buffer<float> filter(3, 3, 8, 1, 1);
+    Buffer<float> bias(8, 1);
+    Buffer<float> adjoint(64, 64, 1, 1);
 
-    for (int y = 0; y < input.height(); y++) {
-        for (int x = 0; x < input.width(); x++) {
-            input(x, y) = rand();
-        }
-    }
-
-    for (int z = 0; z < filter.channels(); z++) {
-        for (int y = 0; y < filter.height(); y++) {
-            for (int x = 0; x < filter.width(); x++) {
-                filter(x, y, z) = rand();
+    for (int n = 0; n < input.dim(3).extent(); n++) {
+        for (int c = 0; c < input.channels(); c++) {
+            for (int y = 0; y < input.height(); y++) {
+                for (int x = 0; x < input.width(); x++) {
+                    input(x, y, c, n) = rand();
+                }
             }
         }
     }
 
-    for (int x = 0; x < bias.width(); x++) {
-        bias(x) = rand();
-    }
-
-    for (int y = 0; y < target.height(); y++) {
-        for (int x = 0; x < target.width(); x++) {
-            target(x, y) = rand();
+    for (int n = 0; n < input.channels(); n++) {
+        for (int y = 0; y < input.height(); y++) {
+            for (int x = 0; x < input.width(); x++) {
+                guide(x, y, n) = rand();
+            }
         }
     }
 
-    Buffer<float> output(64, 64);
-    Buffer<float> d_filter(3, 3, 8);
-    Buffer<float> d_bias(8);
+    for (int co = 0; co < filter.dim(4).extent(); co++) {
+        for (int ci = 0; ci < filter.dim(3).extent(); ci++) {
+            for (int z = 0; z < filter.channels(); z++) {
+                for (int y = 0; y < filter.height(); y++) {
+                    for (int x = 0; x < filter.width(); x++) {
+                        filter(x, y, z, ci, co) = rand();
+                    }
+                }
+            }
+        }
+    }
 
-    bilateral_layer(input, filter, bias, target, output, d_filter, d_bias);
+    for (int c = 0; c < bias.height(); c++) {
+        for (int z = 0; z < bias.width(); z++) {
+            bias(z, c) = rand();
+        }
+    }
+
+    for (int n = 0; n < adjoint.dim(3).extent(); n++) {
+        for (int c = 0; c < adjoint.channels(); c++) {
+            for (int y = 0; y < adjoint.height(); y++) {
+                for (int x = 0; x < adjoint.width(); x++) {
+                    adjoint(x, y, c, n) = rand();
+                }
+            }
+        }
+    }
+
+    Buffer<float> output(64, 64, 1, 1);
+    Buffer<float> d_input(67, 67, 1, 1);
+    Buffer<float> d_guide(67, 67, 1);
+    Buffer<float> d_filter(3, 3, 8, 1, 1);
+    Buffer<float> d_bias(8, 1);
+
+    bilateral_layer(input, guide, filter, bias, adjoint, output, d_input, d_guide, d_filter, d_bias);
 
     // Timing code
 
     // Auto-scheduled version
     double min_t_auto = benchmark(1, 1, [&]() {
-        bilateral_layer(input, filter, bias, target, output, d_filter, d_bias);
+        bilateral_layer(input, guide, filter, bias, adjoint, output, d_input, d_guide, d_filter, d_bias);
     });
     printf("Time: %gms\n", min_t_auto * 1e3);
 
