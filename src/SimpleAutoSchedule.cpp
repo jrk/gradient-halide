115a7f2743985d06e941818698c4da9b9935efe0
Tzu-Mao Li <tzumao@persephone.csail.mit.edu>
2018-01-17 12:20:06 -0500

    change thread num

--- src/SimpleAutoSchedule.cpp
+++ src/SimpleAutoSchedule.cpp
@@ -144,7 +144,7 @@ void simple_autoschedule(std::vector<Func> &outputs,
                 }
                 // Launch GPU threads
                 Var block, thread;
-                func.gpu_tile(fused_vars.back(), block, thread, 1);
+                func.gpu_tile(fused_vars.back(), block, thread, 32);
             }
         }
 
